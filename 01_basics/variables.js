8d265a6435823500e45802c8434437e9b7c47d8e
Harshulverma <136536883+Harshulverma@users.noreply.github.com>
2024-03-25 05:06:22 +0000

    variables define in javascript

---
+++ 01_basics/variables.js
@@ -0,0 +1,17 @@
+const account_id= 14453
+let accountemail ="harshul@gmail.com"
+var accountpassword ="12345"
+accountcity="jaipur"
+
+//account_id=2
+accountemail="harshul34@gamil.com"
+accountpassword="12443334"
+accountcity="jodhpur"
+let accountstate;
+console.log(account_id);
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+console.table([account_id,accountemail,accountpassword,accountcity,accountstate]);
+
