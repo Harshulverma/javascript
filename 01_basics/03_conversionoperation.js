3cabc504c601de68337d51ff1e4ea9d65161b7d3
Harshulverma <136536883+Harshulverma@users.noreply.github.com>
2024-03-25 06:21:24 +0000

    operations

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -10,3 +10,24 @@ let isloggedin=1
 let booleanisloogedin=Boolean(isloggedin)
 console.log(typeof(booleanisloogedin));
 console.log(booleanisloogedin);
+
+//***************operations***************
+let value=3
+let negvalue=-value
+console.log(negvalue);
+console.log(2+2);
+console.log(2*2);
+console.log(2**3);
+
+let str1="hello "
+let str2="harshul"
+
+let str3 =str1 + str2;
+console.log(str3);
+
+let num1 ,num2,num3
+num1=num2=num3=4;
+let gamecounter=100;
+gamecounter++;
+++gamecounter;
+console.log(gamecounter);
