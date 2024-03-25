aa89b41db9f7fa1e3171f1e6fe8798c4dfb2b536
Harshulverma <136536883+Harshulverma@users.noreply.github.com>
2024-03-25 06:00:24 +0000

    conversion

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,12 @@
+let score="33"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueinnumber=Number(score)
+console.log(typeof valueinnumber);
+//"33"->33
+//"33abc"->NaN
+let isloggedin=1
+let booleanisloogedin=Boolean(isloggedin)
+console.log(typeof(booleanisloogedin));
+console.log(booleanisloogedin);
