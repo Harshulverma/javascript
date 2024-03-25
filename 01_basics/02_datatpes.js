0d665af3f0b842633d7f66adc053a7abcf7f601a
Harshulverma <136536883+Harshulverma@users.noreply.github.com>
2024-03-25 05:25:59 +0000

    datatypes

---
+++ 01_basics/02_datatpes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all js code as newer version
+//alert(3+3) we are using noejs ,not browser
+console.log(3+3);
+console.log("harshul");//code readibility should be focused.
+
+let name="harshul"
+let age =20
+let isloggedin=false
+
+//number
+// bigint
+//string =""
+// boolean->true/false
+//null->standalone value
+//undefined -> not declarded yet
+//symbol->unique
+
+// object
+
+console.log(typeof"harshul");
