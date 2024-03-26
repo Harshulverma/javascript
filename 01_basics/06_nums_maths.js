70ab7e4ed9f9ea7e33db74027550dce8c625b6ed
Harshulverma <136536883+Harshulverma@users.noreply.github.com>
2024-03-26 06:23:18 +0000

    number and maths

---
+++ 01_basics/06_nums_maths.js
@@ -0,0 +1,20 @@
+const score=400
+console.log(score);
+
+const balance=new Number(100)
+console.log(balance);
+console.log(balance.toString().length);
+console.log(balance.toFixed(2));
+
+const othernumber =23.85656
+
+console.log(othernumber.toPrecision(3));
+
+const hundreds=1000000
+console.log(hundreds.toLocaleString('en-In'));
+
+//*********************maths************************** */
+
+console.log(Math);
+console.log(math.abs(-4))
+console.log(math.round(4.6));
