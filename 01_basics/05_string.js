bc17468b84155232c8a7e59bfab8643bbe7e0c6f
Harshulverma <136536883+Harshulverma@users.noreply.github.com>
2024-03-26 06:02:48 +0000

    strings

---
+++ 01_basics/05_string.js
@@ -0,0 +1,29 @@
+const name="harshul"
+const marks=50
+
+//console.log(name+marks);
+console.log(`hello my name is ${name} and my marks is ${marks}`);
+
+const gameName =new String('Harshudds')
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf('h'));
+
+const newstring=gameName.substring(0,4)
+console.log(newstring);
+
+const anotherstring =gameName.slice(-8,4)
+console.log(anotherstring);
+
+const newstring1="  hitesh  "
+console.log(newstring1);
+console.log(newstring1.trim());
+
+const url="https://harshul.com/harshul%20verma"
+
+url.replace('%20','-')
+console.log(url.replace('%20','-'));
